6eef891589bb11e7ba5d6013e5b16caab5b496c0
Yash <jainyash0710@gmail.com>
2022-10-14 18:25:45 +0530

    header boilerplate code is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { BasicInfoComponent } from './views/basic-info/basic-info.component';
+import { AboutMeComponent } from './views/about-me/about-me.component';
+import { ResumeComponent } from './views/resume/resume.component';
+import { SkillsComponent } from './views/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasicInfoComponent,
+    AboutMeComponent,
+    ResumeComponent,
+    SkillsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
